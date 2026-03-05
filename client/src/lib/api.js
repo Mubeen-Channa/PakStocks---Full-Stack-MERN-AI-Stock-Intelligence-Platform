52892f45376f3a61d4d91ea688a1a91df806c01b
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2026-03-05 10:04:45 +0500

    Add api.js: Add Axios setup with userId header from localStorage

---
+++ client/src/lib/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:4000/api",
+});
+
+api.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+
+  if (user?.id) {
+    config.headers.userId = user.id;
+  }
+
+  return config;
+});
+
+export default api;
