cf1e83bcb24e9491352408d8d0e053a8adc74f19
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2026-03-21 11:01:27 +0500

    Add **News page** with a simple Coming Soon message

---
+++ client/src/pages/News.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+export default function News() {
+
+  return (
+    <div className="py-6 max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold mt-40 mx-40 w-full text-white">Comming Soon...</h1>     
+    </div>
+  );
+}
