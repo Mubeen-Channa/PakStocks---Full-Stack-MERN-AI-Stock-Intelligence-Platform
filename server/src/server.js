29b82f3205dfa1e6e76712d6949c3757976213fa
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2026-03-26 11:12:03 +0500

    setup dotenv and connectDB with basic async start function

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,15 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config({ quiet: true });
 
-dotenv.config();
+import { connectDB } from "./db/connect.js";
 
-const app = express();
-const PORT = process.env.PORT || 4000;
+async function start() {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error("Failed to start DB", err);
+    process.exit(1);
+  }
+}
 
-// Middleware
-app.use(express.json());
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-// Health check
-app.get("/health", (req, res) => {
-  res.json({ status: "ok", time: new Date().toISOString() });
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+start();
