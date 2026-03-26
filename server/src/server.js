ff55b0d0130b615f35b59722ecff18bae8541fa8
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2026-03-26 11:46:11 +0500

    setup server with DB connection and start Express app

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
+
+import app from "./app.js";
 import { connectDB } from "./db/connect.js";
+
+const PORT = process.env.PORT || 4000;
 
 async function start() {
   try {
     await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
   } catch (err) {
-    console.error("Failed to start DB", err);
+    console.error("Failed to start server", err);
     process.exit(1);
   }
 }
